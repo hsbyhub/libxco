187d619c984ebb6cc00d7bada7d99ccef8ede812
hsby <mac>
2022-02-07 18:21:58 +0800

    设计协程

--- src/coroutine.cpp
+++ src/coroutine.cpp
@@ -5,3 +5,181 @@
       	创建日期：2022/1/27
  *================================================================*/
 #include "coroutine.h"
+#include <cstring>
+#include <assert.h>
+
+X_NAMESPAVE_START
+/**
+ * @brief 协程环境信息
+ */
+const int   g_co_call_stack_max                     = 128;  // 协程调用栈大小
+Coroutine*  g_co_call_stack[g_co_call_stack_max]    = {0};  // 协程调用栈
+int         g_co_call_stack_top                     = -1;   // 协程调用栈栈顶
+Coroutine*  g_pending_co                            = nullptr;
+Coroutine*  g_occupy_co                             = nullptr;
+
+/**
+ * @brief 协程入栈
+ */
+Coroutine* PullCoroutine();
+
+/**
+ * @brief 协程出栈
+ */
+void PushCoroutine(Coroutine* co);
+
+/**
+ * @brief 获取当前协程
+ */
+Coroutine* GetCurrentCoroutine();
+
+Coroutine::SysContext::SysContext(char* _ss_sp,
+           size_t _ss_size,
+           void* cb,
+           void* arg0,
+           void* arg1) : ss_sp(_ss_sp), ss_size(_ss_size){
+
+    // 获取栈底地址(存放着返回地址)
+    char* sp = ss_sp + ss_size - sizeof(void*);
+    sp = (char*)((unsigned long)sp & -16LL);
+
+    // 将栈底存放的返回地址赋值为pfn
+    memset(regs, 0, sizeof(regs));
+    void** ret_addr = (void**)(sp);
+    *ret_addr = (void*)cb;
+
+    regs[kRiRsp] = sp;
+    regs[kRiRetAddr] = ret_addr;
+    regs[kRiRdi] = arg0;
+    regs[kRiRsi] = arg1;
+}
+
+void Coroutine::SysContext::Swap(Coroutine::SysContext *new_sys_ctx) {
+}
+
+void PushCoroutine(Coroutine* co) {
+    assert(g_co_call_stack_top < g_co_call_stack_max - 1);
+    auto cur_co = g_co_call_stack[g_co_call_stack_top];
+    auto pending_co = g_co_call_stack[++g_co_call_stack_top] = co;
+    Coroutine::Swap(cur_co, pending_co);
+}
+
+Coroutine* PullCoroutine() {
+    assert(g_co_call_stack_top > -1);
+    auto cur_co = g_co_call_stack[g_co_call_stack_top];
+    auto pending_co = g_co_call_stack[--g_co_call_stack_top];
+    Coroutine::Swap(cur_co, pending_co);
+    return cur_co;
+}
+
+Coroutine* GetCurrentCoroutine() {
+    if (g_co_call_stack_top == -1) {
+        // 初始化当前为主协程
+        auto cur_co = new Coroutine(nullptr);
+        cur_co->SetState(Coroutine::State::kStExec);
+        PushCoroutine(cur_co);
+    }
+    return g_co_call_stack[g_co_call_stack_top];
+}
+
+Coroutine::Coroutine(CbType* cb, void* cb_arg, int stack_size, Coroutine::StackMem *stack_mem) {
+    if (stack_mem) {
+        stack_mem_ = stack_mem;
+        is_share_stack_mem_ = true;
+    }else {
+        if(stack_size & 0xFFF )
+        {
+            stack_size &= ~0xFFF;
+            stack_size += 0x1000;
+        }
+        stack_mem_ = new StackMem(stack_size);
+        is_share_stack_mem_ = false;
+    }
+    cb_ = cb;
+    cb_arg_ = cb_arg;
+    state_ = State::kStInit;
+}
+
+Coroutine::~Coroutine() {
+    if (!is_share_stack_mem_ && stack_mem_) {
+        delete stack_mem_;
+        stack_mem_ = nullptr;
+    }
+    if (sys_context_) {
+        delete sys_context_;
+        sys_context_ = nullptr;
+    }
+    if (stack_backup_buffer) {
+        free(stack_backup_buffer);
+        stack_backup_buffer = nullptr;
+    }
+}
+
+void* Coroutine::OnCoroutine(void *arg) {
+    assert(state_ == State::kStExec);
+    void* ret = cb_(arg);
+    state_ = State::kStEnd;
+    return ret;
+}
+
+void Coroutine::Resume() {
+    assert(state_ != State::kStEnd);
+    if (!sys_context_) {
+        sys_context_ = new SysContext(stack_mem_->buffer, stack_mem_->size, (void*)cb_, this, cb_arg_);
+    }
+    PushCoroutine(this);
+}
+
+void Coroutine::Yield() {
+    PullCoroutine();
+}
+
+void Coroutine::Swap(Coroutine* cur_co, Coroutine* pending_co) {
+    cur_co->SetState(State::kStHold);
+    pending_co->SetState(State::kStExec);
+    // 获取栈顶指针
+    char c;
+    cur_co->stack_sp_ = &c;
+    if (pending_co->is_share_stack_mem_) {
+        // 新协程的共享栈的使用者
+        auto share_stack_occupy_co = pending_co->stack_mem_->occupy_co;
+        pending_co->stack_mem_->occupy_co = pending_co;
+        g_occupy_co = share_stack_occupy_co;
+        g_pending_co = pending_co;
+        if (share_stack_occupy_co && share_stack_occupy_co != pending_co) {
+            share_stack_occupy_co->BackupStackMem();
+        }
+    }else {
+        g_pending_co = nullptr;
+        g_occupy_co = nullptr;
+    }
+
+    // 切换系统上下文
+    sys_context_swap(cur_co->sys_context_, pending_co->sys_context_);
+
+    // 还原共享栈
+    if (g_occupy_co && g_pending_co && g_occupy_co != g_pending_co && g_pending_co->stack_backup_buffer) {
+        memcpy(g_pending_co->stack_sp_, g_pending_co->stack_backup_buffer, g_pending_co->stack_backup_size);
+    }
+}
+
+Coroutine::State Coroutine::GetState() {
+    return state_;
+}
+
+void Coroutine::SetState(Coroutine::State st) {
+    state_ = st;
+}
+
+void Coroutine::BackupStackMem() {
+    int stack_use_len = stack_mem_->bp - stack_sp_;
+    if (stack_backup_buffer) {
+        free(stack_backup_buffer);
+        stack_backup_buffer = nullptr;
+    }
+    stack_backup_buffer = (char*)malloc(stack_use_len);
+    stack_backup_size = stack_use_len;
+    memcpy(stack_backup_buffer, stack_sp_, stack_backup_size);
+}
+
+X_NAMESPAVE_END
--- src/coroutine.h
+++ src/coroutine.h
@@ -7,18 +7,21 @@
 #pragma once
 
 #include <cstddef>
+#include <memory>
 #include "define.h"
 
 X_NAMESPAVE_START
 
-struct SysContext;
-extern "C" {
-    void sys_context_swap(SysContext* cur_ctx, SysContext* new_ctx) asm("sys_context_swap");
-}
-
 class Coroutine {
 public:
-    using callback_fn_t = void*(void* arg);
+    using CbType = void*(void* arg);
+
+    enum class State{
+        kStInit     = 0,
+        kStExec     = 1,
+        kStHold     = 2,
+        kStEnd      = 3,
+    };
 
     /**
      * @brief 协程系统上下文
@@ -30,25 +33,93 @@ public:
             kRiRetAddr = 9,
             kRiRsp = 13,
         };
-        SysContext(callback_fn_t* cb);
+        SysContext(char* _ss_sp,
+                   size_t _ss_size,
+                   void* cb,
+                   void* arg0 = nullptr,
+                   void* arg1 = nullptr);
+
+        void Swap(SysContext* new_sys_ctx);
+
         void* regs[14];
-        size_t ss_size; // 栈大小
         char* ss_sp;    // 栈内存块
+        size_t ss_size; // 栈大小
     };
 
     /**
      * @brief 运行时栈内存
      */
-    struct RunningStackMem{
-        Coroutine* current_co;  // 当前使用内存块的协程
-        size_t size;            // 使用大小
-        char* buffer;           // 内存块起始地址
-        char* bp;               // 栈底地址, bp = buffer + size
+    struct StackMem{
+        StackMem(int _size) {
+            occupy_co = nullptr;
+            size = _size;
+            buffer = (char*)malloc(size);
+            bp = buffer + size;
+        }
+        ~StackMem() {
+            if (buffer) {
+                free(buffer);
+                buffer = nullptr;
+            }
+        }
+        Coroutine* occupy_co    = nullptr;  // 当前使用内存块的协程
+        size_t size             = 0;        // 使用大小
+        char* buffer            = nullptr;  // 内存块起始地址
+        char* bp                = nullptr;  // 栈底地址, bp = buffer + size
     };
 
+public:
+    /**
+     * @brief 构造函数
+     */
+    Coroutine(CbType* cb, void* arg = nullptr, int stack_size = 128 * 1024, StackMem* stack_mem = nullptr);
+
+    /**
+     * @brief 析构函数
+     */
+    ~Coroutine();
+
+    /**
+     * @brief 协程处理
+     */
+    void* OnCoroutine(void* arg);
+
+    /**
+     * @brief 唤起协程
+     */
+    void Resume();
+
+    State GetState();
+
+    void SetState(State st);
+    
+    /**
+     * @brief 挂起协程
+     */
+    static void Yield();
+    
+    /**
+     * @brief 切换协程
+     */
+    static void Swap(Coroutine* cur_co, Coroutine* pending_co);
+
 private:
-    SysContext sys_context_;            // 协程上下文
-    RunningStackMem running_stack_mem_; // 运行时栈内存
+    void BackupStackMem();
+
+private:
+    StackMem*           stack_mem_          = nullptr;  // 栈内存
+    bool                is_share_stack_mem_ = false;    // 是否共享栈
+    SysContext*         sys_context_        = nullptr;  // 协程上下文
+    CbType*             cb_                 = nullptr;  // 回调
+    void*               cb_arg_             = nullptr;  // 回调参数
+    State               state_;                         // 协程状态
+    char*               stack_sp_           = nullptr;  // 栈指针(共享栈的备份)
+    int                 stack_backup_size   = 0;        // 栈备份大小
+    char*               stack_backup_buffer = nullptr;  // 栈备份
 };
+
+extern "C" {
+void sys_context_swap(xlib::Coroutine::SysContext* cur_ctx, xlib::Coroutine::SysContext* new_ctx) asm("sys_context_swap");
+}
 
 X_NAMESPAVE_END
